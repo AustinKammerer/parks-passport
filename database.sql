0c508464a93a6b05c0ca5ee12e59b5aff191586e
Austin Kammerer <austin4191@gmail.com>
2021-11-15 11:43:04 -0600

    database setup

--- database.sql
+++ database.sql
@@ -8,3 +8,22 @@ CREATE TABLE "user" (
     "username" VARCHAR (80) UNIQUE NOT NULL,
     "password" VARCHAR (1000) NOT NULL
 );
+
+-- table to hold the trip logs for all the users
+CREATE TABLE "trip" (
+    "id" SERIAL PRIMARY KEY,
+    "user_id" INT NOT NULL REFERENCES "user" ON DELETE CASCADE,
+    "park_key" VARCHAR (20) NOT NULL,
+    "image_path" VARCHAR (510),
+    "is_current" BOOLEAN NOT NULL DEFAULT FALSE,
+    "is_complete" BOOLEAN NOT NULL DEFAULT FALSE
+);
+
+-- table to hold the actual log entries users make for each trip
+CREATE TABLE "log" (
+    "id" SERIAL PRIMARY KEY,
+    "trip_id" INT NOT NULL REFERENCES "trip" ON DELETE CASCADE,
+    "type" VARCHAR (20) NOT NULL,
+    "text" VARCHAR (1000),
+    "image_path" VARCHAR (510)
+);
