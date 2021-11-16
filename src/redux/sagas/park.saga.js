b914483d35d5c97c9be9af9f1a6de821e415834e
Austin Kammerer <austin4191@gmail.com>
2021-11-15 20:06:44 -0600

    reducer to store park search results

--- src/redux/sagas/park.saga.js
+++ src/redux/sagas/park.saga.js
@@ -3,12 +3,17 @@ import { put, takeLatest } from "redux-saga/effects";
 
 // will make get request to NPS API based on the state selected
 function* fetchSearchResults(action) {
+  // user selected state to search for parks
   const state = action.payload;
   try {
+    // GET request to /finder which then sends a GET to the NPS API with a state query
+    // response is a list of parks in that state
     const searchResults = yield axios.get(
       `/api/parks/finder?stateCode=${state}`
     );
     console.log("search results:", searchResults.data);
+    // send the response to the searchResults reducer
+    yield put({ type: "SET_RESULTS", payload: searchResults.data });
   } catch (error) {
     console.log("error getting search results:", error);
   }
