c8e05a59682759c8a2c0332bfa6fdc1716595b64
Austin Kammerer <austin4191@gmail.com>
2021-11-23 10:18:41 -0600

    added columns and functions/triggers for adding timestamps to trip and log

--- database.sql
+++ database.sql
@@ -18,7 +18,9 @@ CREATE TABLE "trip" (
     "park_code" VARCHAR (20) NOT NULL,
     "is_current" BOOLEAN NOT NULL DEFAULT FALSE,
     "is_complete" BOOLEAN NOT NULL DEFAULT FALSE,
-    "image_path" VARCHAR (510)
+    "image_path" VARCHAR (510),
+    "started_at" TIMESTAMPTZ,
+    "ended_at" TIMESTAMPTZ
 );
 
 -- table to hold the actual log entries users make for each trip
@@ -28,5 +30,38 @@ CREATE TABLE "log" (
     "trip_id" INT NOT NULL REFERENCES "trip" ON DELETE CASCADE,
     "type" VARCHAR (20) NOT NULL,
     "text" VARCHAR (1000),
-    "image_path" VARCHAR (510) DEFAULT NULL
+    "image_path" VARCHAR (510),
+    "time" TIMESTAMPTZ NOT NULL DEFAULT NOW()
 );
+
+-- function to create a timestamp when a trip is started
+CREATE OR REPLACE FUNCTION trigger_start_timestamp()
+RETURNS TRIGGER AS $$
+BEGIN
+   NEW.started_at = now(); 
+   RETURN NEW;
+END;
+$$ language 'plpgsql';
+
+-- triggers the above function when is_current is set to true
+CREATE TRIGGER update_started_at 
+BEFORE UPDATE ON "trip"
+FOR EACH ROW
+WHEN (NEW.is_current = true)
+EXECUTE PROCEDURE trigger_start_timestamp();
+
+-- function to create a timestamp when a trip is ended
+CREATE OR REPLACE FUNCTION trigger_end_timestamp()
+RETURNS TRIGGER AS $$
+BEGIN
+   NEW.ended_at = now(); 
+   RETURN NEW;
+END;
+$$ language 'plpgsql';
+
+-- triggers the above function when is_complete is set to true
+CREATE TRIGGER update_ended_at 
+BEFORE UPDATE ON "trip"
+FOR EACH ROW
+WHEN (NEW.is_complete = true)
+EXECUTE PROCEDURE trigger_end_timestamp();
